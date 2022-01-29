6c068160c7a52caa5fb116055a6b0d02771f6424
woozxn <squib0612@gmail.com>
2022-01-30 05:24:27 +0900

    Entity Class 개발
    Member, Order, Delivery, OrderItem, Item, Address

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable //어딘가에 내장될 수 있다
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
